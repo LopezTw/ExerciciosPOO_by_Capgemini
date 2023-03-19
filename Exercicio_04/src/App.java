f4e1d90c9f194aef986f80e48f9d03986be9e2c5
Caio Augusto <55853613+LopezTw@users.noreply.github.com>
2023-03-19 16:13:18 -0300

    "Exercício Concluído"

--- Exercicio_04/src/App.java
+++ Exercicio_04/src/App.java
@@ -1,7 +1,7 @@
 /** Crie uma classe denominada Elevador para armazenar as informações de um elevador dentro de um 
 prédio. A classe deve armazenar o andar atual (térreo = 0), total de andares no prédio (desconsiderando o 
-térreo), capacidade do elevador e quantas pessoas estão presentes nele. A classe deve também 
-disponibilizar os seguintes métodos:
+térreo), capacidade do elevador e quantas pessoas estão presentes nele. 
+A classe deve também disponibilizar os seguintes métodos:
 
 a. Inicializa: que deve receber como parâmetros a capacidade do elevador e o total de andares no 
 prédio (os elevadores sempre começam no térreo e vazio);
